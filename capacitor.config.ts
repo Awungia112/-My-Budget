b27b6e458ea38553ac8f9714caa96af2751f533f
micheal-ndoh <michaelndoh9@gmail.com>
2026-01-16 10:09:58 +0100

    Update iOS deployment target to 13.0 and regenerate platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.vercel.my_budget_amber',
   appName: 'My Budget',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    deploymentTarget: '13.0'
+  }
 };
 
 export default config;
